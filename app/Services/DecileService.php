a890d6f7098657cfbc6f41735d23191160376d9c
yuta_miki_wakayama <mikiyuta7991@gmail.com>
2023-04-17 06:56:12 +0900

    decile

--- app/Services/DecileService.php
+++ app/Services/DecileService.php
@@ -1,55 +1,15 @@
 <?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Inertia\Inertia;
-use App\Models\Order;
+namespace App\Services;
 use Illuminate\Support\Facades\DB;
 
-
-class AnalysisController extends Controller
+class DecileService
 {
-    public function index()
-    {
-
-        $startDate = '2022-08-01';
-        $endDate = '2022-08-31';
-
-        // $period = Order::betweenDate($startDate,$endDate)
-        // ->groupBy('id')
-        // ->selectRaw('id,sum(subtotal) as total,
-        // customer_name,status,created_at')
-        // ->orderBy('created_at')
-        // ->paginate(50);
-
-        //dd($period);
-        // $subQuery = Order::betweenDate($startDate,$endDate)
-        // ->where('status',true)
-        // ->groupBy('id')
-        // ->selectRaw('id,sum(subtotal) as totalPerPurchase,
-        // DATE_FORMAT(created_at, "%Y%m%d") as date');
-
-        // $data = DB::table($subQuery)
-        // ->groupBy('date')
-        // ->selectRaw('date ,sum(totalPerPurchase) as total')
-        // ->get();
-
-
-
-        return Inertia::render('Analysis');
-    }
-
-    public function decile()
-    {
-        
-        $startDate = '2022-08-01';
-        $endDate = '2022-08-31';
-
-                // RFM分析
+  public static function decile($subQuery)
+  {
+           // RFM分析
         // 1. 購買ID毎にまとめる 
-        $subQuery = Order::betweenDate($startDate, $endDate)
-        ->groupBy('id')
+        $subQuery = $subQuery->groupBy('id')
         ->selectRaw('id, customer_id, customer_name, 
         SUM(subtotal) as totalPerPurchase, created_at');
 
@@ -125,6 +85,11 @@ class AnalysisController extends Controller
         totalRatio
         ')->get(); 
 
-        //dd($data);
-    }
+        
+    $labels = $data->pluck('decile');
+    $totals = $data->pluck('totalPerGroup');
+
+    return [$data,$labels,$totals];
+  }
+  
 }
